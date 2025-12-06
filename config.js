8ee70de4d654a235ce7283a727fb56fa80cc5cd3
panbapz <99779474+panbapz@users.noreply.github.com>
2025-12-07 00:07:38 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+export const PASSWORD = "1234";
+
+export const noData = [
+    { text: "Anh chắc chưa đó?", btn: "Thật sao anh", img: "./assets/images/2.gif" },
+    { text: "Cục cưng ơi", btn: "Anh chắc chưa đó?", img: "./assets/images/3.gif" },
+    { text: "Đừng làm vậy với em", btn: "Đừng làm vậy với Em", img: "./assets/images/4.gif" },
+    { text: "Tim em đang tan vỡ", btn: "Tim em đang tan vỡ", img: "./assets/images/5.gif" },
+    { text: "Em sắp khóc rồi đó", btn: "Em sắp khóc rồi đó", img: "./assets/images/6.gif" }
+];
+
+export const letterHeading = "Gửi em,";
+
+export const letterText = `Cảm ơn em đã bước đến một cách mãnh liệt nhưng cũng dịu dàng đến thế.`;
